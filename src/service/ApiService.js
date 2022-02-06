197204a2253f1f024ded39ece9cae8959b0b9f05
nahwasa-local <nahwasa@gmail.com>
2022-02-06 12:34:59 +0900

    JWT로 백엔드와 연동 가능하도록 함. 로그인 화면 추가. gitignore 추가.

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,16 +1,44 @@
 import {API_BASE_URL} from "../app-config";
 import axios from "axios";
 
+const ACCESS_TOKEN_KEY = "ACCESS_TOKEN";
+
+const getCallHeaders = () => {
+    let headers = {
+        "Content-Type": "application/json"
+    };
+
+    const accessToken = localStorage.getItem(ACCESS_TOKEN_KEY);
+    if (accessToken && accessToken != null) {
+        headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return headers;
+}
+
 export const call = (api, method, request={}) =>
     axios({
         baseURL: API_BASE_URL,
         url: api,
         method: method,
-        data: request
+        data: request,
+        headers: getCallHeaders()
     }).catch((e) => {
+        console.log("error : " + e);
         alert("로그인이 필요합니다.");
         if (e.response.status === 403) {
             window.location.href = "/login";
         }
     });
 
+export const signin = (userDTO) => {
+    (async () => {
+        let response = await call("/auth/signin", "POST", userDTO);
+        console.log("response : ", response);
+
+        if (response.data.token) {
+            localStorage.setItem(ACCESS_TOKEN_KEY, response.data.token);
+            window.location.href = "/";
+        }
+    })();
+}
